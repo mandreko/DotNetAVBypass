2705713e9d45e3bd2a6b4cd0a1dddfc2c2062ddc
Matt Andreko <mandreko@gmail.com>
2012-07-20 10:44:37 -0400

    Cleanup
    Fixed the output filenames.
    Removed the un-needed app.config

--- DotNetAVBypass/Properties/AssemblyInfo.cs
+++ DotNetAVBypass/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Wrapper")]
+[assembly: AssemblyTitle("DotNetAVBypass")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Wrapper")]
+[assembly: AssemblyProduct("DotNetAVBypass")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
